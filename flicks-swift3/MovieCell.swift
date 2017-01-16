7e0d9226a573cf008f45075baf1ab4eea59b759f
kmolo <kmoloney@mail.ccsf.edu>
2017-01-15 22:07:38 -0800

    create IBOutlet for Title + Label in MovieCell.swift

--- flicks-swift3/MovieCell.swift
+++ flicks-swift3/MovieCell.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
