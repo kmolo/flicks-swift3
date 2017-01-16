01992a9db7071c683bf8ed432aa563a8e07788e9
kmolo <kmoloney@mail.ccsf.edu>
2017-01-15 22:04:46 -0800

    set custom class of MovieCell to MovieCell in Identity Inspection

---
+++ flicks-swift3/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flicks-swift3
+//
+//  Created by mny on 1/15/17.
+//  Copyright © 2017 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
